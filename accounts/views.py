e021e21dc6e6ed0b7581cd12fe90a4c15dfbf033
tomprete <prete316@gmail.com>
2022-12-27 17:08:26 -0600

    added stripe

--- accounts/views.py
+++ accounts/views.py
@@ -11,14 +11,17 @@ from rest_framework.decorators import api_view
 import json
 import os
 import stripe
+from mixpanel import Mixpanel
+
+mp = Mixpanel('c9b89c7bf5d74371eaa2dbf629c20821')
 
 stripe.api_key = 'sk_test_51LTVzmCxk3VOyNJUcsZ4S3O5C7y1p6tLcLw37L17reSYaZyIdSlUxMMKkboTgXo0sePUsYoJ5QdSEVvqiDAHJv6G00e0wdArHg'
+
+endpoint_secret = 'whsec_ba7c25d90b4323378364b4c666d220db1656797062019a8906503f12a044e513'
 
 development = True
 
 FRONTEND_DOMAIN_URL = "https://app.workouttoday.co/" if development == False else 'http://localhost:3000/'
-
-print(FRONTEND_DOMAIN_URL)
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -29,7 +32,6 @@ class CustomUserAuth(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request, format=None):
-        print(request.user)
         if request.user.is_authenticated:
             return Response({
                 'username': request.user.username,
@@ -43,14 +45,11 @@ class CustomUserAuth(APIView):
 
 def create_stripe_customer(user):
     customer = stripe.Customer.create(email=user.email)
-    print("CUSTOMER: ", customer)
-    update_user_with_stripe_id(customer)
+    update_user_with_stripe_id(customer, user)
     return customer
 
-def update_user_with_stripe_id(stripe_customer):
+def update_user_with_stripe_id(stripe_customer, user):
     try:
-        user_email = stripe_customer.email
-        user = User.objects.get(email=user_email)
         user.stripe_id=stripe_customer.id
         user.save()
         return True
@@ -58,8 +57,11 @@ def update_user_with_stripe_id(stripe_customer):
        return False
 
 def get_stripe_customer(user):
-    customer = stripe.Customer.retrieve(user.stripe_id)
-    return customer
+    try:
+        customer = stripe.Customer.retrieve(user.stripe_id)
+        return customer
+    except Exception as e:
+        return e
 
 def get_subscription_key(subscription_type):
     if subscription_type == 'monthly':
@@ -69,8 +71,18 @@ def get_subscription_key(subscription_type):
     return price_id
 
 def find_customer_by_email(email):
-    user = User.objects.get(email=email)
-    return user
+    try:
+        user = User.objects.get(email=email)
+        return user
+    except Exception as e:
+        return e
+
+def find_customer_by_stripe_id(stripe_id):
+    try:
+        user = User.objects.get(stripe_id=stripe_id)
+        return user
+    except Exception as e:
+        return e
 
 def update_user_with_stripe_customer_id(user, stripe_id):
     user.stripe_id = stripe_id
@@ -82,17 +94,63 @@ class SubscriptionWebhook(APIView):
     permission_classes = []
 
     def post(self, request, format=None):
-        req = json.load(request)
-        if req['type'] == 'customer.created':
-            email = req['data']['object']['email']
-            stripe_customer_id = req['data']['object']['id']
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+        payload = request.body.decode('utf-8')
+        try:
+            event = stripe.Webhook.construct_event(
+                    payload, sig_header, endpoint_secret
+                )
+        except ValueError as e:
+            # Invalid payload
+            return Response({'message': 'value error'}, status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            print('Invalid signature')
+            return Response({'message': f"SignatureVerificationError: {e}"}, status=400)
+
+        if event['type'] == 'customer.created':
+            email = event['data']['object']['email']
+            stripe_customer_id = event['data']['object']['id']
             user = find_customer_by_email(email)
             update_user_with_stripe_customer_id(user, stripe_customer_id)
-        elif req['type'] == 'customer.subscription.created':
-            print(req['data'])
-        elif req['type'] == 'invoice.payment_succeeded':
-            print('invoice.payment_succeeded: ', req)
+        elif event['type'] == 'customer.subscription.created':
+            print('customer.subscription.created: ', event['data'])
+        elif event['type'] == 'invoice.payment_succeeded':
+            email = event['data']['object']['customer_email']
+            user = find_customer_by_email(email)
+            if user.is_premium != True:
+                user.is_premium = True
+                user.save()
+        elif event['type'] == 'payment_intent.succeeded':
+            stripe_id = event['data']['object']['customer']
+            user = find_customer_by_stripe_id(stripe_id)
+            if user.is_premium != True:
+                user.is_premium = True
+                user.save()
+            else:
+                print("Customer already is premium")
+        elif event['type'] == 'customer.subscription.deleted':
+            stripe_id = event['data']['object']['customer']
+            user = find_customer_by_stripe_id(stripe_id)
+            if user.is_premium == True:
+                user.is_premium = False
+                user.save()
+            else:
+                print("Customer already is premium")
+        elif event['type'] == 'payment_intent.payment_failed':
+            payment_intent = event['data']['object']
 
+            # Use stored information to get an error object
+            e = payment_intent['last_payment_error']
+
+            # Use its type to choose a response
+            mp.track(e['payment_method']['customer'], 'card_declined')
+            if e['type'] == 'card_error':
+                print("A payment error occurred: {}".format(e['message']))
+            elif e['type'] == 'invalid_request':
+                print("An invalid request occurred.")
+            else:
+                print("Another problem occurred, maybe unrelated to Stripe")
 
 
 
@@ -107,18 +165,16 @@ class CheckoutSession(APIView):
 
     def post(self, request, format=None):
         checkout_info = json.load(request)
-        print('user: ', request.user)
         try:
-            print(checkout_info)
+            # print('checkout_info: ', checkout_info)
             if request.user.stripe_id:
                 stripe_customer = get_stripe_customer(request.user)
             else:
                 stripe_customer = create_stripe_customer(request.user)
-            print("STRIPE CUSTOMER: ", stripe_customer)
             price = stripe.Price.retrieve(
                 get_subscription_key(checkout_info)
             )
-            print("PRICE: ", price)
+            # print("PRICE: ", price)
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
@@ -126,16 +182,16 @@ class CheckoutSession(APIView):
                         'quantity': 1,
                     },
                 ],
-                subscription_data={
-                    'trial_period_days': 7
-                },
+                # subscription_data={
+                #     'trial_period_days': 7
+                # },
                 automatic_tax={
                     'enabled': False
                 },
                 mode='subscription',
                 customer=stripe_customer.id,
-                success_url=f"{FRONTEND_DOMAIN_URL}checkout" +
-                '?success=true&session_id={CHECKOUT_SESSION_ID}',
+                success_url=f"{FRONTEND_DOMAIN_URL}login" +
+                '?success=true',
                 cancel_url=f"{FRONTEND_DOMAIN_URL}login" + '?canceled=true',
             )
             data = {
@@ -154,13 +210,62 @@ class CustomerPortalSession(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def post(self, request):
-        print(request.user)
+        # print(request.user)
         user_info = json.load(request)
-        print("EMAIL: ", user_info)
+        # print("EMAIL: ", user_info)
         try:
             stripe_id = request.user.stripe_id
             customer = get_stripe_customer(request.user)
-            print("CUSTOMER: ", customer)
+            # subscription = stripe.Subscription.list(customer=stripe_id)
+            # print("SUBSCRIPTION: ", subscription)
+            portal_configuration = stripe.billing_portal.Configuration.create(
+                business_profile={
+                    "headline": "Cactus Practice partners with Stripe for simplified billing.",
+                },
+                features={
+                    "invoice_history": {
+                        "enabled": True
+                        },
+                    "payment_method_update": {
+                        "enabled": True
+                        },
+                    "subscription_cancel": {
+                        "cancellation_reason": {
+                            "enabled": True,
+                            "options": [
+                                'too_expensive',
+                                'missing_features',
+                                'switched_service',
+                                'unused',
+                                'too_complex',
+                                'low_quality',
+                                'other'
+                                ]
+                        },
+                        "enabled": True,
+                        "mode": "at_period_end",
+                        "proration_behavior": "none"
+                        },
+                    "subscription_pause": {
+                        "enabled": True
+                        },
+                    "subscription_update": {
+                        "default_allowed_updates": ["price"],
+                        "products": [
+                            {
+                                "product": "prod_MqPztshwi4bDEH",
+                                "prices": ["price_1M6jJPCxk3VOyNJUwyFqanWS"]
+                            },
+                            {
+                                "product": "prod_MqPwuMNDjnHJWJ",
+                                "prices": ["price_1M6j70Cxk3VOyNJUHyaJ4IQ5"]
+                            }
+                        ],
+                        "enabled": True,
+                        "proration_behavior": "none"
+                        }
+                    }
+                )
             # This is the URL to which the customer will be redirected after they are
             # done managing their billing with the portal.
             return_url = f"{FRONTEND_DOMAIN_URL}"
@@ -168,9 +273,14 @@ class CustomerPortalSession(APIView):
             portalSession = stripe.billing_portal.Session.create(
                 customer=customer,
                 return_url=return_url,
+                configuration=portal_configuration
             )
-            print("portal: ", portalSession)
-            return redirect(portalSession.url, code=303)
+            # print("portal: ", portalSession)
+            data = {
+                'stripe_url': portalSession.url
+            }
+            # print("DATA: ", data)
+            return Response(data, status=200)
         except Exception as e:
             print("EXCEPTION: ", e)
             data = {
