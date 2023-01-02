36a8a4e3d66811d47001c3b80cd93c320f426c33
tomprete <prete316@gmail.com>
2023-01-02 17:15:27 -0600

    updated config

--- workout_today/settings.py
+++ workout_today/settings.py
@@ -13,10 +13,12 @@ import django_heroku
 from pathlib import Path
 import os
 from datetime import timedelta
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -104,7 +106,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 WSGI_APPLICATION = 'workout_today.wsgi.application'
 
